8e44271ce163c839cec0c99475f09ac7e5ebbd60
Alice279 <tianrun279@163.com>
2022-03-16 21:40:10 +0800

    (feat): init component card

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Card from '../../componens/Card'
+import './index.css'
 
 function About () {
     return (
-        <div>
-            
+        <div className="about">
+            <Card />
         </div>
     )
 }
