0c3e9632c00f4673d5ac345294993a431cc72457
Alice279 <tianrun279@163.com>
2022-03-29 20:57:21 +0800

    (feat): add background of homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,7 @@ import Button from '../../componens/Button'
 import Circle from '../../componens/Circle'
 import './index.css'
 import { cardContext } from '../App'
+import Particles from 'react-tsparticles'
 
 function Home() {
 
@@ -19,6 +20,52 @@ function Home() {
 
     return (
         <div className="home">
+            <Particles params={{
+                particles: {
+                    number: {
+                        value: 1,
+                        density: {
+                            enable: true,
+                            value_area: 10,
+                        },
+                    },
+                    color: {
+                        value: {r: 255, g: 230, b: 128}
+                    },
+                    shape: {
+                        type: "star"
+                    },
+                    move: {
+                        enable: true,
+                        direction: "bottom",
+                        speed: 0.7,
+                    },
+                    size: {
+                        value: 2.7,
+                    },
+                    opacity: {
+                        anim: {
+                            enable: true,
+                            speed: 1.5,
+                            opacity_min: 0.07,
+                        },
+                    },
+                },
+                interactivity: {
+                    events: {
+                        onclick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                    },
+                    modes: {
+                        push: {
+                            particles_nb: 1,
+                        },
+                    },
+                },
+                retina_detect: true,
+            }} />
             <Circle className="circle" />
             <Button className="button" Click={handleClick} place="home">Come on!</Button>
         </div>
