29c8d082ec76c2da0a9264b7b3182f2d4c70bbe8
Alice279 <tianrun279@163.com>
2022-04-03 19:55:18 +0800

    (feat): finish add delete pull message from database

--- src/pages/Message/index.js
+++ src/pages/Message/index.js
@@ -1,38 +1,87 @@
-import React, { useReducer, useRef } from 'react'
+import React, { useReducer, useRef, useEffect, useState } from 'react'
+import axios from 'axios'
 import Button from '../../componens/Button'
 import './index.css'
 
 function Message() {
     const inputRef = useRef();
+    //useReducer 包括了上一次渲染的内容，多次渲染之间不会重新加载
     const [items, dispatch] = useReducer((state, action) => {
         switch (action.type) {
             case 'add':
                 return [...state, {
-                    id: state.length,
-                    name: action.name
+                    content: action.content,
+                    id: action.id
                 }]
             case 'delet':
                 return state.filter((item) => {
                     return item.id !== action.id
                 })
+            case 'pull':
+                return [...action.arr]
+            case 'clear':
+                return []
         }
     }, [])
 
+    //提交一个新的留言时，先把之前展示出来的留言清空再从数据库里重新拉取留言
+    //这是因为items和数据库里的留言不是一个体系的
     const handlePub = (event) => {
-        event.preventDefault();
-        dispatch({
-            type: 'add',
-            name: inputRef.current.value
+        axios({
+            method: 'POST',
+            url: 'http://localhost:9000/testAPI/register',
+            data: {
+                content: inputRef.current.value,
+                id: Date.now()
+            }
         })
-        inputRef.current.value = '';
+            .then(res => res.data)
+            .then(res => {
+                dispatch({
+                    type: 'add',
+                    content: res.content,
+                    id: res.id
+                })
+            })
+        inputRef.current.value = ''
     }
+
     const handleDel = (event, id) => {
         event.preventDefault();
+        axios({
+            method: 'DELETE',
+            url: 'http://localhost:9000/testAPI/delRegisterInfo',
+            data: {
+                id
+            }
+        })
+            .then(dispatch({
+                type: 'delet',
+                id
+            }))
+    }
+
+    const addPreMessage = (arr) => {
+        console.log('addPreMessage')
         dispatch({
-            type: 'delet',
-            id
+            type: 'pull',
+            arr
         })
     }
+
+    useEffect(() => {
+        console.log('sueEffect')
+        axios({
+            method: 'GET',
+            url: 'http://localhost:9000/testAPI/getRegisterInfo'
+        })
+            .then(res => res.data.data)
+            // .then(res => console.log(res))    
+            //这里注意，使用了 console.log 等代码之后，函数默认返回值是 undefined
+            //再给下面的 then 传入的也是 undefined
+            .then(res => addPreMessage(res))
+    }, [])
+
     return (
         <div className="message">
             <div className="message-add">
@@ -45,7 +94,7 @@ function Message() {
             <div className="message-show">
                 {items.map(item => (
                     <div key={item.id} className="one-message">
-                        <div className="message-content">{item.name}</div>
+                        <div className="message-content">{item.content}</div>
                         <Button className="del-button" size="xsmall" Click={(event) => handleDel(event, item.id)}>D</Button>
                     </div>
                 ))}
